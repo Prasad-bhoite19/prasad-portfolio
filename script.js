7d59cabd676608b57af543a5267e6b9ef94eb9ed
Prasad Bhoite <prasadsb2002@gmail.com>
2025-10-10 23:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// ===== Typing Effect for Tagline =====
-const text = "Cloud & DevOps Engineer | AWS | Linux | Docker | Python";
-let index = 0;
-const speed = 100; // typing speed in ms
-
-function typeWriter() {
-  if (index < text.length) {
-    document.getElementById("tagline").innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-window.onload = () => {
-  typeWriter();
-};
 let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
 
@@ -30,5 +14,19 @@ window.addEventListener("scroll", function() {
   
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For mobile or negative scrolling
 });
+// ===== Typing Effect for Tagline =====
+const text = "Cloud & DevOps Engineer | AWS | Linux | Docker | Python";
+let index = 0;
+const speed = 100; // typing speed in ms
 
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("tagline").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
+window.onload = () => {
+  typeWriter();
+};
